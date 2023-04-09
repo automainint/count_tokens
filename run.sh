4eb35723ed6b8522035faf7130ce95503d4680de
Mitya Selivanov <automainint@guattari.tech>
2023-04-09 05:33:54 +0200

    Fix run script

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@ i=0
 while [ "$i" -lt 30 ]; do
   file=$(printf "%02d.jsonl" "$i")
   url=$(printf "https://the-eye.eu/public/AI/pile/train/%02d.jsonl.zst" "$i")
-  echo "$i Download ${url}"
+  printf "* %02d  Download ${url}..." "$i"
   wget "${url}"
-  echo "$i Extract archive"
-  unstzip "${file}.zst" "${file}"
+  printf "* %02d  Extract archive" "$i"
+  unzstd "${file}.zst" -o "${file}"
   rm "${file}.zst"
-  echo "$i Process data..."
+  printf "* %02d  Process data..." "$i"
   ./count_tokens "${file}" temp.bin result.csv
   rm "${file}"
-  echo "$i Done"
+  printf "* %02d  Done" "$i"
   i=$((i + 1))
 done
 echo "All done"

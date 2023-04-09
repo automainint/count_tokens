abb76f2c3359c4f66f38e84824152d5c9f1c1bc5
Mitya Selivanov <automainint@guattari.tech>
2023-04-09 18:40:03 +0200

    Update logging style

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@ i=0
 while [ "$i" -lt 30 ]; do
   file=$(printf "%02d.jsonl" "$i")
   url=$(printf "https://the-eye.eu/public/AI/pile/train/%02d.jsonl.zst" "$i")
-  printf "* %02d  Download ${url}...\n\n" "$i"
+  printf "* %02d  Download ${url}\n" "$i"
   rm -f "${file}.zst"
   wget -q "${url}"
-  printf "* %02d  Extract archive\n\n" "$i"
+  printf "* %02d  Extract archive\n" "$i"
   unzstd --rm "${file}.zst" -o "${file}"
-  printf "* %02d  Process data...\n" "$i"
+  printf "* %02d  Process data\n" "$i"
   ./count_tokens "${file}" temp.bin result.csv
   rm "${file}"
-  printf "* %02d  Done\n\n" "$i"
+  printf "* %02d  Done\n" "$i"
   i=$((i + 1))
 done
 echo "All done"

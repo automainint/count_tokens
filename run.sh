4e0ca80ce6321f5fb8b465754bc8b618282ba7ee
Mitya Selivanov <69725676+automainint@users.noreply.github.com>
2023-04-09 05:42:57 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,9 @@ while [ "$i" -lt 30 ]; do
   url=$(printf "https://the-eye.eu/public/AI/pile/train/%02d.jsonl.zst" "$i")
   printf "* %02d  Download ${url}...\n\n" "$i"
   rm -f "${file}.zst"
-  wget "${url}"
+  wget -q "${url}"
   printf "* %02d  Extract archive\n\n" "$i"
-  unzstd "${file}.zst" -o "${file}"
-  rm "${file}.zst"
+  unzstd --rm "${file}.zst" -o "${file}"
   printf "* %02d  Process data...\n" "$i"
   ./count_tokens "${file}" temp.bin result.csv
   rm "${file}"
